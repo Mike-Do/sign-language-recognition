f26f21aaaac9f196a5f1a343401b2c792b559aea
Mike-Do <agentdonoso@gmail.com>
2022-05-02 22:37:05 -0400

    Update preprocessing

--- preprocess.py
+++ preprocess.py
@@ -2,51 +2,107 @@
 import os
 import cv2
 import numpy as np
-import utils
 import json
+
+import shutil
+
+# dictionary maps glosses to their corresponding frames
+all_frames = {}
 
 # This is the method for splitting all video samples into frames.
 def videosToFrames():
     directory = 'videos'
-    all_frames = []
     
-    # iterate over files in the directory
-    for filename in os.listdir(directory):
-        f = os.path.join(directory, filename) # file path
-        if os.path.isfile(f):
-            # create a VideoCapture object to read the video
-            cap = cv2.VideoCapture(f)
-            curr_frames = []
+    # if the frames directory does not exist, create it
+    # if not os.path.exists('frames'):
+    #     os.makedirs('frames')
+    
+    # iterate over the sub directories in videos
+    for gloss in os.listdir(directory):
+        gloss_frames = [] # frames for all videos in the same gloss
 
-            # Loop until the end of the video or until the threshold has been reached
-            while (cap.isOpened()):
-                # capture frame by frame, ret will be None if the frame is not valid
-                ret, frame = cap.read()
-                
-                # check if the frame is valid
-                if ret:
-                    # resize the frame
+        sub_dir = os.path.join(directory, gloss)
+        # iterate over files in the directory
+        for filename in os.listdir(sub_dir):
+            f = os.path.join(sub_dir, filename) # file path
+            if os.path.isfile(f):
+                # create a VideoCapture object to read the video
+                cap = cv2.VideoCapture(f)
+                curr_frames = []
+
+                # get the total number of frames in the video
+                total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+                print("Total frames for " + filename + " is " + str(total_frames) + " frames.")
+
+                # Loop until the end of the video or until the threshold has been reached
+                while (cap.isOpened()):
+                    # capture frame by frame, ret will be None if the frame is not valid
+                    ret, frame = cap.read()
+                    
+                    # break if the frame is not valid
+                    if not ret:
+                        break
+                    
+                    # resize the frame to a smaller size and add it to the list of frames
                     frame = cv2.resize(frame, (540, 380), fx=0, fy=0, interpolation=cv2.INTER_CUBIC)
-                    # add the current frame to the list of frames
                     curr_frames.append(frame)
-            
-            # add all the frames to the list of all frames        
-            all_frames.append(curr_frames)
-            
-            # release the video capture object
-            cap.release()
+                    
+                # release the video capture object
+                cap.release()
+                
+                # add all the frames to the list of all frames        
+                gloss_frames.append(curr_frames)
 
-    frame_indices = make_dataset()
-    selected_frames = []
+        # if not os.path.exists('frames/' + gloss):
+        #     os.makedirs('frames/' + gloss)
+
+        # print("Saving frames for " + filename)
+
+        # # write the frames to the frames directory
+        # for i in range(len(gloss_frames)):
+        #     for j in range(len(gloss_frames[i])):
+        #         # use OpenCv to write the frames to the gloss sub directory
+        #         cv2.imwrite('frames/' + gloss + '/' + filename + '_' + str(i) + '.jpg', gloss_frames[i][j])
+        
+        # add all the frames to the all_frames dictionary
+        all_frames[gloss] = gloss_frames
     
-    # for each video in the dataset
-    for index in range(len(frame_indices)):
-        # get the selected frames in the video
-        video_frames = all_frames[index][frame_indices[index]]
-        # add the selected frames to the list of selected frames
-        selected_frames.append(video_frames)
+    print("Printing all frames")
+    print(all_frames.keys())
 
+    
+    frame_indices = make_dataset()
+    selected_frames = {}
+
+    # all_frames - key: gloss, value: list of frames
+    # frame_indices - key: gloss, value: list of lists (indices)
+    # selected_frames - key: gloss, value: list of the selected frames
+
+    # for each gloss in frame indices
+    for gloss, frames_index in frame_indices.items():
+        print("List of indices is: " + str(len(frames_index)) + " long.")
+        # for each list of indices in the gloss
+        for i in range(len(frames_index)):
+            # for each index in the list of indices
+            print("Length of frames: " + str(len(all_frames[gloss])))
+            print("Index is " + str(i))
+            print("Frames are: " + str(all_frames[gloss][i]))
+            
+            selected_frames[gloss] = all_frames[gloss][frame_indices[gloss]]
+
+        # selected = [all_frames[gloss][i] for i in len(frames_index)]
+        # selected_frames[gloss] = selected #TODO: Change if doesn't work
+    
+    print("Printing number of selected frames")
+    print(len(selected_frames.values()))
+    # # for each video in the dataset
+    # for index in range(len(frame_indices)):
+    #     # get the selected frames in the video
+    #     video_frames = all_frames[index][frame_indices[index]]
+    #     # add the selected frames to the list of selected frames
+    #     selected_frames.append(video_frames)
     return selected_frames
+    
 
 """"
 This function loops through all objects in the dataset
@@ -57,7 +113,7 @@ and stores the indices of all frames in the all_frames list.
 """
 def make_dataset():
     # store video instances into custom entries
-    data = []
+    data = {}
     # set the split file
     split = ['train', 'val']
     # set the number of samples per video
@@ -65,7 +121,7 @@ def make_dataset():
     # store the directory of the videos
     index_file_path = "./WLASL_v0.3.json"
     # store the list of all videos
-    all_frames = []
+    all_videos = {}
 
     # open the json file and read into content
     with open(index_file_path, 'r') as f:
@@ -76,33 +132,56 @@ def make_dataset():
         # store the gloss and the video instances (there are multiple videos for each gloss)
         gloss, instances = gloss_entry['gloss'], gloss_entry['instances']
 
-        # for each video instance
-        for instance in instances:
-            # if the video is not in the train split
-            if instance['split'] not in split:
-                # skip the video instance
-                continue
-            
-            # store the frame end and start, as well as the video id
-            frame_end = instance['frame_end']
-            frame_start = instance['frame_start']
-            video_id = instance['video_id']
+        # only create entires for desired glosses
+        if (gloss == "hello" or gloss == "world"):
+            gloss_data = []
+            # for each video instance
+            for instance in instances:
+                # if the video is not in the train split
+                if instance['split'] not in split:
+                    # skip the video instance
+                    continue
+                
+                # store the frame end and start, as well as the video id
+                frame_end = instance['frame_end']
+                frame_start = instance['frame_start']
+                video_id = instance['video_id']
 
-            # store the id, frame start, frame end as an entry in the data list
-            instance_entry = video_id, frame_start, frame_end
-            data.append(instance_entry)
+                # store the id, frame start, frame end as an entry in the data list
+                instance_entry = video_id, frame_start, frame_end
+                gloss_data.append(instance_entry)
+
+            data[gloss] = gloss_data
     
+    # go through each gloss and get its frames
+    for index in data.keys():
+        gloss_frames = []
+        # for each data instance in gloss_data
+        for datum in data[index]: 
+            # destructure the data entry stored above
+            video_id, frame_start, frame_end = datum
+            # sequential sampling the frames
+            frames = sequential_sampling(frame_start, frame_end, num_samples)
+            gloss_frames.append(frames)
+        
+        all_videos[index] = gloss_frames
+
+
     # go through each video and get its frame samples
-    for index in range(len(data)):
-        # destructure the data entry stored above
-        video_id, frame_start, frame_end = data[index]
-        # sequential sampling the frames
-        frames = sequential_sampling(frame_start, frame_end, num_samples)
-        # store all the grabbed frames
-        all_frames.append(frames)
+    
+    # for index in range(len(data)):
+    #     # destructure the data entry stored above
+    #     video_id, frame_start, frame_end = data[index]
+    #     # sequential sampling the frames
+    #     frames = sequential_sampling(frame_start, frame_end, num_samples)
+    #     # store all the grabbed frames
+    #     all_videos.append(frames)
     
     # return the list of the indices all frames selected
-    return all_frames
+
+    print(all_videos)
+
+    return all_videos
     
 
 """Keep sequentially ${num_samples} frames from the whole video sequence by uniformly skipping frames."""
